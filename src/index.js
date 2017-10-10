57e32eb446569f089779f69c87ade6321f4cc213
Lettisia <lettisiag@gmail.com>
2017-10-10 16:12:38 +1100

    Added list of questions

--- src/index.js
+++ src/index.js
@@ -22,6 +22,80 @@ const question = {
   answer: '3',
 };
 
+const questions = [
+  {
+    question: ' 2 + 2 = ',
+    right: 3,
+    left: 4,
+    top: 5,
+    bottom: 2,
+    answer: 4,
+  }, {
+    question: '3 + 4 = ',
+    right: 3,
+    left: 6,
+    top: 7,
+    bottom: 1,
+    answer: 7,
+  }, {
+    question: '1 + 6 = ',
+    right: 1,
+    left: 4,
+    top: 7,
+    bottom: 9,
+    answer: 7,
+  }, {
+    question: '2 + 3 = ',
+    right: 5,
+    left: 4,
+    top: 6,
+    bottom: 3,
+    answer: 5,
+  }, {
+    question: '1 + 4 = ',
+    right: '1',
+    left: '4',
+    top: '5',
+    bottom: '3',
+    answer: '5',
+  }, {
+    question: '4 + 2 = ',
+    right: '8',
+    left: '6',
+    top: '7',
+    bottom: '4',
+    answer: '6',
+  }, {
+    question: '3 + 5 = ',
+    right: '8',
+    left: '6',
+    top: '7',
+    bottom: '4',
+    answer: '8',
+  }, {
+    question: '6 + 4 = ',
+    right: '9',
+    left: '10',
+    top: '12',
+    bottom: '8',
+    answer: '10',
+  }, {
+    question: '4 + 5 = ',
+    right: '8',
+    left: '9',
+    top: '4',
+    bottom: '11',
+    answer: '9',
+  }, {
+    question: '8 + 3 = ',
+    right: '8',
+    left: '9',
+    top: '13',
+    bottom: '11',
+    answer: '11',
+  },
+];
+
 const pages = [
   { path: '/', Component: HomeScreen },
   { path: '/contacts', Component: ContactScreen, props: { contacts } },
@@ -30,10 +104,9 @@ const pages = [
   { path: '/elevatorpitch', Component: ElevatorPitchScreen },
   { path: '/welldone', Component: WellDoneScreen },
   { path: '/yourpage', Component: YourPage },
-  { path: '/question', Component: QuestionScreen, props: { ...question } },
+  { path: '/question', Component: QuestionScreen, props: { ...question, questions } },
   { path: '/welcome', Component: WelcomeScreen },
 ];
 
 ReactDOM.render(
-  <WatchApp pages={ pages } />,
-  document.getElementById('root'));
+  <WatchApp pages={ pages } />, document.getElementById('root'));
