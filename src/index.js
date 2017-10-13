390f8b7e9720336501b213a274f5d3c82d5eac35
Lettisia <lettisiag@gmail.com>
2017-10-14 10:26:47 +1100

    Moar Questions

--- src/index.js
+++ src/index.js
@@ -12,11 +12,6 @@ import ConfirmationScreen from './app/pages/ConfirmationScreen/ConfirmationScree
 import YourPage from './app/pages/PlaySpace/YourPage';
 import QuestionScreen from './app/pages/QuestionScreen/QuestionScreen';
 import WelcomeScreen from './app/pages/WelcomeScreen/WelcomeScreen';
-<<<<<<< Updated upstream
-import ActivityScreen from './app/pages/ActivityScreen/ActivityScreen';
-import ResultScreen from './app/pages/ResultScreen/ResultScreen';
-import questions from './app/data/questions.json';
-=======
 import FinalScreen from './app/pages/FinalScreen/FinalScreen';
 
 const question = {
@@ -27,81 +22,7 @@ const question = {
   right: '4',
   answer: '3',
 };
-//
-// const questions = [
-//   {
-//     question: ' 2 + 2 = ',
-//     right: 3,
-//     left: 4,
-//     top: 5,
-//     bottom: 2,
-//     answer: 4,
-//   }, {
-//     question: '3 + 4 = ',
-//     right: 3,
-//     left: 6,
-//     top: 7,
-//     bottom: 1,
-//     answer: 7,
-//   }, {
-//     question: '1 + 6 = ',
-//     right: 1,
-//     left: 4,
-//     top: 7,
-//     bottom: 9,
-//     answer: 7,
-//   }, {
-//     question: '2 + 3 = ',
-//     right: 5,
-//     left: 4,
-//     top: 6,
-//     bottom: 3,
-//     answer: 5,
-//   }, {
-//     question: '1 + 4 = ',
-//     right: '1',
-//     left: '4',
-//     top: '5',
-//     bottom: '3',
-//     answer: '5',
-//   }, {
-//     question: '4 + 2 = ',
-//     right: '8',
-//     left: '6',
-//     top: '7',
-//     bottom: '4',
-//     answer: '6',
-//   }, {
-//     question: '3 + 5 = ',
-//     right: '8',
-//     left: '6',
-//     top: '7',
-//     bottom: '4',
-//     answer: '8',
-//   }, {
-//     question: '6 + 4 = ',
-//     right: '9',
-//     left: '10',
-//     top: '12',
-//     bottom: '8',
-//     answer: '10',
-//   }, {
-//     question: '4 + 5 = ',
-//     "right": "8",
-//     left: '9',
-//     top: '4',
-//     bottom: '11',
-//     answer: '9',
-//   }, {
-//     question: '8 + 3 = ',
-//     right: '8',
-//     left: '9',
-//     top: '13',
-//     bottom: '11',
-//     answer: '11',
-//   },
-// ];
->>>>>>> Stashed changes
+
 
 const pages = [
   { path: '/', Component: HomeScreen },
@@ -111,18 +32,9 @@ const pages = [
   { path: '/elevatorpitch', Component: ElevatorPitchScreen },
   { path: '/confirmation', Component: ConfirmationScreen, props: { ...questions } },
   { path: '/yourpage', Component: YourPage },
-<<<<<<< Updated upstream
-  { path: '/question',
-    Component: QuestionScreen,
-    props: { ...questions[Math.floor(Math.random() * questions.length)] } },
-  { path: '/welcome', Component: WelcomeScreen },
-  { path: '/activity', Component: ActivityScreen },
-  { path: '/result', Component: ResultScreen },
-=======
   { path: '/question', Component: QuestionScreen, props: { ...question } },
   { path: '/welcome', Component: WelcomeScreen },
   { path: '/final', Component: FinalScreen },
->>>>>>> Stashed changes
 ];
 
 ReactDOM.render(
