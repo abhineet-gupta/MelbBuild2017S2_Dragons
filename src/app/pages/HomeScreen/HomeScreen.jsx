9089cc91b04f6614a9fb8e5a5dad2982de22adf3
Monali Ghosh <monalighosh@users.noreply.github.com>
2017-10-14 13:28:32 +1100

    Disabled a left button on homescreen

--- src/app/pages/HomeScreen/HomeScreen.jsx
+++ src/app/pages/HomeScreen/HomeScreen.jsx
@@ -123,7 +123,7 @@ export const HomeScreenComponent = () => {
 };
 
 export const HomeScreenButtons = {
-  LEFT: () => ButtonAction.goToPage({ pathname: '/counter', state: { number: 5 } }),
+  LEFT: () => {},
   RIGHT: () => ButtonAction.goToPage('/welcome'),
   TOP: () => ButtonAction.scrollUp(),
   BOTTOM: () => ButtonAction.scrollDown(),
